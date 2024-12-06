4d5399ce7cb4eb5a5d5475aac1d9216478419f92
bebelac <177419747+daisybeverly@users.noreply.github.com>
2024-12-06 22:47:32 +0700

    Update tailwind.config.mjs

--- sites/tailwind.config.mjs
+++ sites/tailwind.config.mjs
@@ -7,39 +7,58 @@ export default {
   ],
   darkMode: "class",
   theme: {
+    extend: {
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '3.75rem',
+        '7xl': '4.5rem',
+        '8xl': '6rem',
+        '9xl': '8rem',
+      },
+      spacing: {
+        '128': '32rem',
+      }
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
-      black: "#212121",
-      white: "#f5f5f5",
+      black: colors.black,
+      white: colors.white,
       gray: colors.gray,
       indigo: colors.indigo,
       neutral: colors.neutral,  // Used mainly for text color
       green: {
-        30: "#1f7a1f",
-        40: "#29a329",
-        50: "#33cc33",
-        60: "#5cd65c",
-        70: "#85e085",
+        300: colors.emerald[500],
+        400: colors.emerald[600],
+        500: colors.emerald[700],
+        600: colors.emerald[800],
+        700: colors.emerald[900],
       },
       yellow: {
-        50: "#fefce8",
-        100: "#fef9c3",
-        400: "#facc15",
-        500: "#eab308",
+        50: colors.amber[50],
+        100: colors.amber[100],
+        400: colors.amber[400],
+        500: colors.amber[500],
       }, // Accent colors, used mainly for star color, heading and buttons
       orange: {
-        100: "#ffedd5",
-        200: "#fed7aa",
-        300: "#fb713b",
-        400: "#fa5a15",
-        500: "#e14d0b",
-        600: "#ea580c",
+        100: colors.orange[100],
+        200: colors.orange[200],
+        300: colors.orange[300],
+        400: colors.orange[400],
+        500: colors.orange[500],
+        600: colors.orange[600],
       }, // Primary colors, used mainly for links, buttons and svg icons
       red: colors.red, // Used for bookmark icon
       zinc: colors.zinc, // Used mainly for box-shadow
     },
-    extend: {},
   },
   plugins: [
     require("tailwindcss/nesting"),
